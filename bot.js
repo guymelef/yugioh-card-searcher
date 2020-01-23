321fdd0efc6dd30db6ceca0e6d262bd082608c03
guymelef <jerson.mari@gmail.com>
2020-01-23 14:47:57 +0800

    Fix bug

--- bot.js
+++ bot.js
@@ -31,7 +31,9 @@ const options = {
     "domainvalidators",
     "thesandvich",
     "cardsearcher",
-    "nifroth"  ]
+    "nifroth",
+    "steven_freshy"
+  ]
 }
 
 const client = new tmi.client(options)
@@ -73,7 +75,7 @@ function onMessageHandler (channel, userState, message, self) {
               if (cards.length === 1) {
                 sendInfoForOneCard(cards[0], channel)
               } else if (cards.length > 100) {
-                client.say(channel, `@${userState.username}, your search yielded more than ${cards.length} cards. Try refining your search query.`)
+                client.say(channel, `@${userState.username}, your search yielded ${cards.length} cards. Try refining your search query.`)
               } else {
                 const found = cards.find(card => card.name.toLowerCase() === commandArg)
                 if (found) {
