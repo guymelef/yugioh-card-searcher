dc7b716758d5d1b6cbb5f72fa1b6fdd5fd831571
guymelef <jerson.mari@gmail.com>
2020-01-24 21:11:29 +0800

    Edit response format to prevent BTTV from replacing emojis. Add condition for channels that want to make the command available for all

--- bot.js
+++ bot.js
@@ -29,12 +29,16 @@ const options = {
   },
   channels: [
     "domainvalidators",
-    "thesandvich",
     "cardsearcher",
+    "thesandvich",
     "nifroth",
-    "mcblueskies"
+    "mcblueskies",
   ]
 }
+
+const unModeratedChannels = [
+  '#nifroth',
+]
 
 const client = new tmi.client(options)
 
@@ -50,42 +54,42 @@ function onConnectedHandler (server, port) {
 
 function onMessageHandler (channel, userState, message, self) {
   if (self) return
-  
-  if ((options.channels.includes(`#${userState.username}`) || userState.mod)) {
+
+  if (channel === `#${userState.username}` || userState.mod) {
     const messageArray = message.split(' ')
     const command = messageArray[0].toLowerCase()
     const commandArg = messageArray.slice(1).join(' ').toLowerCase()
 
     switch (command) {
       case "!search":
-        if (commandArg.length === 0) {
-          client.say(channel, "❓ To search for cards, follow this syntax: !card <full/partial card name>")
+        if (!messageArray[1]) {
+          return client.say(channel, "❓ To search for cards, follow this syntax: !card <full/partial card name>")
         } else if (messageArray[1] === "--guide") {
-          client.say(channel, `MONSTER: [💛: Normal, 🧡: Effect, 💙: Ritual, 💜: Fusion, 🤍: Synchro, 🖤: XYZ, 🌗: Pendulum, 🔗: Link, 🃏: Token], 💚: SPELL, ❤️: TRAP, ✨: SKILL`)
+          return client.say(channel, `MONSTER: [💛: Normal, 🧡: Effect, 💙: Ritual, 💜: Fusion, 🤍: Synchro, 🖤: XYZ, 🌗: Pendulum, 🔗: Link, 🃏: Token], 💚: SPELL, ❤️: TRAP, ✨: SKILL`)
         } else if (messageArray[1] === "--random") {
           fetch('https://db.ygoprodeck.com/api/v5/randomcard.php')
             .then(card => card.json())
             .then(card => {
-              sendInfoForOneCard(card[0], channel)
+              return sendInfoForOneCard(card[0], channel)
             })
         } else {
           fetch(`https://db.ygoprodeck.com/api/v5/cardinfo.php?fname=${commandArg}`)
             .then(cards => cards.json())
             .then(cards => {
               if (cards.length === 1) {
-                sendInfoForOneCard(cards[0], channel)
+                return sendInfoForOneCard(cards[0], channel)
               } else if (cards.length > 100) {
-                client.say(channel, `@${userState['display-name']}, your search yielded ${cards.length} cards! Refine your search query and try again.`)
+                return client.say(channel, `@${userState['display-name']}, your search yielded ${cards.length} cards! Refine your search query and try again.`)
               } else {
                 const found = cards.find(card => card.name.toLowerCase() === commandArg)
                 if (found) {
-                  sendInfoForOneCard(found, channel)
+                  return sendInfoForOneCard(found, channel)
                 } else {
                   const cardsArray = cards.map(card => {                
                     const symbol = getSymbol(card.type)
-                    return `${symbol} ${card.name}`
+                    return `${symbol}${card.name}`
                   })
-                  client.say(channel, `📜 [${cards.length} Cards] : ${cardsArray.join(', ')}`)
+                  return client.say(channel, `📜 [${cards.length} Cards] : ${cardsArray.join(', ')}`)
                 }
               }
             })
@@ -126,12 +130,12 @@ const sendInfoForOneCard = (card, channel) => {
 
   if (type.includes("Monster")) {
     cardInfo = `
-      ${card.name} (${card.attribute}) ${card.level ? `[${card.level}⭐]`: ''} [${card.race}/${card.type}] [ATK/${card.atk} ${card.def ? `DEF/${card.def}`: ''}] : ${card.desc}
+      ${card.name} (${card.attribute}) ${card.level ? `[${card.level}⭐]`: ''} [${card.race}/${card.type}] [ATK/${card.atk}${card.def ? ` DEF/${card.def}`: ''}] : ${card.desc}
     `
     const symbol = getSymbol(card.type)
-    cardInfo = `${symbol} ${cardInfo}`
+    cardInfo = `{${symbol}} ${cardInfo}`
   } else {
-    cardInfo = `${cardSymbols[type[0]]} ${card.name} [${card.race} ${card.type}] : ${card.desc}`
+    cardInfo = `{${cardSymbols[type[0]]}} ${card.name} [${card.race} ${card.type}] : ${card.desc}`
   }
 
 
