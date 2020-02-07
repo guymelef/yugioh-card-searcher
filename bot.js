c8fb402c5725737c3de12a95a8f5624bbc34c8d7
guymelef <jerson.mari@gmail.com>
2020-02-07 17:50:36 +0800

    Edit --throw command message

--- bot.js
+++ bot.js
@@ -201,8 +201,8 @@ function onMessageHandler (channel, userState, message, self) {
             .then(card => card.json())
             .then(card => {
               return card.name.includes("Exodia") ?
-              client.action(channel, `: DarkMode SAY GOODBYE TO EXODIAAA!!!1 ${userName.slice(1)} has thrown your cards off the boat, ${channelViewers[randomUser]}! DarkMode`)
-              : client.action(channel, `: Say goodbye to your "${card.name}" card, ${channelViewers[randomUser]}! ${userName.slice(1)} has thrown it off the boat! DarkMode`)
+              client.action(channel, `: PowerUpL DarkMode PowerUpR  SAY GOODBYE TO EXODIAAAAA!!!1 ${userName.slice(1)} has thrown ${channelViewers[randomUser]}'s cards off the boat! PowerUpL DarkMode PowerUpR `)
+              : client.action(channel, `: ${userName.slice(1)} throws ${channelViewers[randomUser]}'s "${card.name}" card off the boat! PowerUpL DarkMode PowerUpR`)
             })
             .catch(err => client.say(channel, `${userName}, there was an error. Try again.`))
           })
