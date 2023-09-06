5c2cff93005e75f8f6f36757ab61cfa2dccf385f
Sandvich <24429906+guymelef@users.noreply.github.com>
2023-09-06 21:56:57 +0800

    add model for cards with unofficial translation

--- models/card.js
+++ models/card.js
@@ -17,9 +17,10 @@ cardSchema.plugin(uniqueValidator)
 const OcgCard = mongoose.model('OcgCard', cardSchema)
 const RushCard = mongoose.model('RushCard', cardSchema)
 const StrayCard = mongoose.model('StrayCard', cardSchema)
+const UnofficialCard = mongoose.model('UnofficialCard', cardSchema)
 
 
 
 
 
-module.exports = { OcgCard, RushCard, StrayCard }
+module.exports = { OcgCard, RushCard, StrayCard, UnofficialCard }
